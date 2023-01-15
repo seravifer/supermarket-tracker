736649a550452a4ccf02f5e5adb30db850c64a45
seravifer <sergiavilafernandez@gmail.com>
2023-01-15 17:56:38 +0100

    feat: create mercadona extension

---
+++ extension/script.js
@@ -0,0 +1,117 @@
+let observer = null;
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (observer != null) {
+      observer.disconnect();
+      observer = null;
+    }
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+        observer = null;
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+let previousUrl = "";
+function onURLChange(callback) {
+  let observerUrl = new MutationObserver(() => {
+    if (location.href !== previousUrl) {
+      console.log("URL changed", location.href);
+      previousUrl = location.href;
+      callback();
+    }
+  });
+  observerUrl.observe(document, { subtree: true, childList: true });
+}
+
+onURLChange(() => {
+  checkPrice();
+});
+
+function checkPrice() {
+  waitForElm("[data-test='private-product-detail-info']").then(async (elm) => {
+    const productId = location.href.split("/")[4];
+
+    const response = await fetch(
+      `https://us-central1-markettracker-85aa0.cloudfunctions.net/article?id=${productId}&company=mercadona`,
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    );
+    const product = await response.json();
+    const today = product.histories.at(-1);
+    product.histories.push({
+      ...today,
+      timestamp: new Date().toISOString(),
+    });
+    const chart = document.createElement("canvas");
+    chart.id = "chart";
+    chart.style = "margin-top: 20px;";
+    elm.appendChild(chart);
+
+    new Chart(chart, {
+      type: "line",
+      data: {
+        labels: product.histories.map((row) => new Intl.DateTimeFormat("es-ES").format(new Date(row.timestamp))),
+        datasets: [
+          {
+            label: "Precio",
+            data: product.histories.map((row) => row.price),
+            borderColor: "#fcb831",
+          },
+          {
+            label: "Precio por unidad",
+            data: product.histories.map((row) => row.bulkPrice),
+            borderColor: "#229e6b",
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            ticks: {
+              callback: (value) => {
+                return new Intl.NumberFormat("es-ES", {
+                  style: "currency",
+                  currency: "EUR",
+                  maximumFractionDigits: 0,
+                }).format(value);
+              },
+            },
+          },
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (context) => {
+                let label = context.dataset.label || "";
+                if (label) {
+                  label += ": ";
+                }
+                if (context.parsed.y !== null) {
+                  label += new Intl.NumberFormat("es-ES", { style: "currency", currency: "EUR" }).format(
+                    context.parsed.y
+                  );
+                }
+                return label;
+              },
+            },
+          },
+        },
+      },
+    });
+  });
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,17 +2,21 @@ import * as functions from "firebase-functions";
 import { db } from "../db.js";
 
 export const article = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, POST");
+  if (req.method === "OPTIONS") {
+    res.status(204).send("");
+    return;
+  }
   const companyId = req.query.id as string;
   const company = req.query.company as string;
-  console.log("id", companyId);
   if (!companyId || !company) {
     res.status(400).send("Missing params");
     return;
   }
   const article = await db.product.findFirst({
     where: {
-      companyId,
-      //company,
+      AND: [{ companyId }, { company }],
     },
     include: {
       histories: true,
